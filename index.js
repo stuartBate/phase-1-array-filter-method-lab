98f2b8256e7c67af45d1d4ad1acc75577deb9b41
Stuart Bate <bate.stuart93@gmail.com>
2023-10-12 13:02:18 -0700

    completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array, target){
+    return array.filter(
+        (possibleMatach) => possibleMatach.toLowerCase() === target.toLowerCase()
+    );
+}
+
+
+function fuzzyMatch(array, testString ){
+    return array.filter(
+        (possibleMatch) =>
+          possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+ }
+    
+
+ function matchName(array, targetName){
+    return array.filter((record) => record.name === targetName);
+ }
